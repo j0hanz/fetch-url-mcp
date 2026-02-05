9db845b255f754ea516f09ad24d30dbe535d7adb
j0hanz <golfarenlj@gmail.com>
2026-02-05 16:25:07 +0100

    refactor: update default Accept-Encoding headers to remove 'br' support

--- src/fetch.ts
+++ src/fetch.ts
@@ -1298,7 +1298,7 @@ const DEFAULT_HEADERS = {
   Accept:
     'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
   'Accept-Language': 'en-US,en;q=0.5',
-  'Accept-Encoding': 'gzip, deflate, br',
+  'Accept-Encoding': 'gzip, deflate',
   Connection: 'keep-alive',
 } as const satisfies Record<string, string>;
 
